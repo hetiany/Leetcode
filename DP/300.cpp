a91c9c9fa415afb039051a9bc4a55ec31ba931c4
hetiany <yanghetian1987@gmail.com>
2016-11-29 01:16:12 -0800

    update

--- DP/300.cpp
+++ DP/300.cpp
@@ -43,3 +43,44 @@ public:
     return maxLen;
  }
 };
+
+//Binary Search, Java, Jiuzhang
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] minLast = new int[nums.length + 1];
+        minLast[0] = Integer.MIN_VALUE;
+        for (int i = 1; i <= nums.length; i++) {
+            minLast[i] = Integer.MAX_VALUE;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            // find the first number in minLast > nums[i]
+            int index = binarySearch(minLast, nums[i]);
+            minLast[index] = nums[i];
+        }
+        
+        for (int i = nums.length; i >= 1; i--) {
+            if (minLast[i] != Integer.MAX_VALUE) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+    private int binarySearch(int[] minLast, int num) {
+        int start = 0, end = minLast.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (minLast[mid] >= num) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (minLast[start] >= num) {
+            return start;
+        }
+        return end;
+    }
+}
--- Greedy/45.cpp
+++ Greedy/45.cpp
@@ -8,13 +8,34 @@ public:
         for(int i = 1; i < sum; ++i) {
             for(int j = 0; j < i; ++j) {
                 if(steps[j] != INT_MAX && j + nums[j] >= i) {
-                    steps[i] = steps[j] + 1;
+                    steps[i] = min(steps[i], steps[j] + 1);
                     break;
                 }
             }
         }
         return steps[sum - 1];
     }
+};
+
+//my code, 11/28/2016
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(!nums.size()) {
+            return 0;
+        }
+        int n = nums.size();
+        vector<int> record(n, INT_MAX);
+        record[0] = 0;
+        for(int i = 1; i < n; ++i) {
+            for(int j = 0; j < i; ++j) {
+                if(record[j] != INT_MAX && nums[j] + j >= i) {
+                    record[i] = min(record[i], record[j] + 1);
+                }
+            }
+        }
+        return record[n-1];
+    } 
 };
 
 
